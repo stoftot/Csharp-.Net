f74a96dd1602c9e06785c3ad3e598bebeb1ea7af
stoftot <gryn@itu.dk>
2025-02-08 19:28:07 +0100

    Created data accses project and restructured

---
+++ WebApp-api-sqlserver/Web.DataAccess/Data/Repositories/UniversityReposetory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web.Api.Data;
+using Web.Api.DTO;
+using Web.Api.Models;
+
+namespace Web.Api.Repositories;
+
+public class UniversityRepository(UniversityDbContext context)
+{
+    private readonly UniversityDbContext context = context;
+
+    //Subject
+    public async Task<IEnumerable<SubjectDTO>> GetAllSubjects() =>
+        await context.Subjects
+            .Select(s => new SubjectDTO { Code = s.Code, Name = s.Name })
+            .ToListAsync();
+    
+    //ClassRoom
+    public async Task<IEnumerable<ClassRoomDTO>> GetAllClassRooms() =>
+        await context.ClassRooms
+            .Select(c => new ClassRoomDTO()
+            {
+                Code = c.Code,
+                Capacity = c.Capacity
+            })
+            .ToListAsync();
+}
