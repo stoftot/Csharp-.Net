13066fb6a199dc9f96c8cacfaac7670fba74d434
stoftot <gryn@itu.dk>
2025-02-08 19:50:41 +0100

    Created subject reposetory

---
+++ WebApp-api-sqlserver/Web.DataAccess/Data/Repositories/SubjectRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web.DataAccess.DTO;
+using Web.DataAccess.Models;
+
+namespace Web.DataAccess.Data.Repositories;
+
+public class SubjectRepository(UniversityDbContext context)
+{
+    public async Task<IEnumerable<SubjectDTO>> GetAllSubjects() =>
+        await context.Subjects
+            .Select(s => new SubjectDTO { Code = s.Code, Name = s.Name })
+            .ToListAsync();
+
+    public async Task<IEnumerable<SubjectDTO>> GetSubject(string code) =>
+        await context.Subjects
+            .Where(s => s.Code == Subject.NormalizeCode(code))
+            .Select(s => new SubjectDTO { Code = s.Code, Name = s.Name })
+            .ToListAsync();
+
+    public async void CreateSubject(SubjectDTO subjectDto)
+    {
+        var subject = new Subject
+        {
+            Code = subjectDto.Code,
+            Name = subjectDto.Name
+        };
+
+        await context.Subjects.AddAsync(subject);
+        await context.SaveChangesAsync();
+    }
+}
--- WebApp-api-sqlserver/Web.DataAccess/Models/Subject.cs
+++ WebApp-api-sqlserver/Web.DataAccess/Models/Subject.cs
@@ -4,4 +4,9 @@ public class Subject
 {
     public string Code { get; set; }
     public string Name { get; set; }
+
+    public static string NormalizeCode(string code)
+    {
+        return code.ToUpper();
+    }
 }
