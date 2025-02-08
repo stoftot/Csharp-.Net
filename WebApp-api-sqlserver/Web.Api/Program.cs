ed185af80a97c6dec630d489ffdd6d5ee2c4b181
stoftot <gryn@itu.dk>
2025-02-08 23:30:32 +0100

    Set up program
    Added maping, controlers and scoped repos

--- WebApp-api-sqlserver/Web.Api/Program.cs
+++ WebApp-api-sqlserver/Web.Api/Program.cs
@@ -1,17 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using Web.DataAccess.Data;
+using Web.DataAccess.Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(); 
 
 builder.Services.AddDbContext<UniversityDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<SubjectRepository>();
+builder.Services.AddScoped<ClassRoomRepository>();
+builder.Services.AddScoped<TeacherRepository>();
+builder.Services.AddScoped<CourseRepository>();
 
 var app = builder.Build();
 
@@ -23,5 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
