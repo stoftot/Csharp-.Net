5ee77013073356ef9215a1f5e3c0b0079dba7058
stoftot <gryn@itu.dk>
2025-02-08 19:54:26 +0100

    Created classroom reposetory

---
+++ WebApp-api-sqlserver/Web.DataAccess/Data/Repositories/ClassRoomRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web.DataAccess.DTO;
+using Web.DataAccess.Models;
+
+namespace Web.DataAccess.Data.Repositories;
+
+public class ClassRoomRepository(UniversityDbContext context)
+{
+    public async Task<IEnumerable<ClassRoomDTO>> GetAllClassRooms() =>
+        await context.ClassRooms
+            .Select(c => new ClassRoomDTO()
+            {
+                Code = c.Code,
+                Capacity = c.Capacity
+            })
+            .ToListAsync();
+    
+    public async Task<IEnumerable<ClassRoomDTO>> GetClassRoom(string code) =>
+        await context.ClassRooms
+            .Where(c => c.Code == ClassRoom.NormalizeCode(code))
+            .Select(c => new ClassRoomDTO()
+            {
+                Code = c.Code,
+                Capacity = c.Capacity
+            })
+            .ToListAsync();
+
+    public async void CreateClassRoom(ClassRoomDTO classRoomDto)
+    {
+        var classRoom = new ClassRoom
+        {
+            Code = classRoomDto.Code,
+            Capacity = classRoomDto.Capacity
+        };
+
+        await context.ClassRooms.AddAsync(classRoom);
+        await context.SaveChangesAsync();
+    }
+}
--- WebApp-api-sqlserver/Web.DataAccess/Models/ClassRoom.cs
+++ WebApp-api-sqlserver/Web.DataAccess/Models/ClassRoom.cs
@@ -4,4 +4,6 @@ public class ClassRoom
 {
     public string Code { get; set; }
     public int Capacity { get; set; }
+
+    public static string NormalizeCode(string code) => code.ToUpper();
 }
