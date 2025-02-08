f9a4b926660292cb1bc9c3cc845199ed50d3ab0e
stoftot <gryn@itu.dk>
2025-02-08 15:34:42 +0100

    Created university dbcontext

---
+++ WebApp-api-sqlserver/Web.Api/Data/UniversityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web.Api.Models;
+
+namespace Web.Api.Data;
+
+public class UniversityDbContext : DbContext
+{
+    public DbSet<Teacher> Teachers { get; set; }
+    public DbSet<Subject> Subjects { get; set; }
+    public DbSet<ClassRoom> ClassRooms { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    
+    public UniversityDbContext(DbContextOptions dbContextOptions)
+    : base(dbContextOptions)
+    {
+        
+    }
+}
--- WebApp-api-sqlserver/Web.Api/Program.cs
+++ WebApp-api-sqlserver/Web.Api/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Web.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<UniversityDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
