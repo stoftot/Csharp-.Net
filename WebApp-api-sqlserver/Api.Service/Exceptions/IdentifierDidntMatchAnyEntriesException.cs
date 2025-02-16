f52ebd6b72ea8ef27bb02b8d1d1f395d5960ed15
stoftot <gryn@itu.dk>
2025-02-16 16:46:04 +0100

    Created costume excpetions

---
+++ WebApp-api-sqlserver/Api.Service/Exceptions/IdentifierDidntMatchAnyEntriesException.cs
@@ -0,0 +1,20 @@
+﻿namespace Api.Service.Exceptions;
+
+public class IdentifierDidntMatchAnyEntriesException : Exception
+{
+    public string? Identifier { get; }
+    
+    public IdentifierDidntMatchAnyEntriesException() { }
+
+    public IdentifierDidntMatchAnyEntriesException(string message)
+        : base(message) { }
+
+    public IdentifierDidntMatchAnyEntriesException(string message, Exception inner)
+        : base(message, inner) { }
+    
+    public IdentifierDidntMatchAnyEntriesException(string message, string identifier)
+        : this(message)
+    {
+        Identifier = identifier;
+    }
+}
---
+++ WebApp-api-sqlserver/Api.Service/Exceptions/MultipleEntriesWhitSameIdentifierException.cs
@@ -0,0 +1,20 @@
+﻿namespace Api.Service.Exceptions;
+
+public class MultipleEntriesWhitSameIdentifierException : Exception
+{
+    public string? Identifier { get; }
+    
+    public MultipleEntriesWhitSameIdentifierException() { }
+
+    public MultipleEntriesWhitSameIdentifierException(string message)
+        : base(message) { }
+
+    public MultipleEntriesWhitSameIdentifierException(string message, Exception inner)
+        : base(message, inner) { }
+
+    public MultipleEntriesWhitSameIdentifierException(string message, string identifier)
+        : this(message)
+    {
+        Identifier = identifier;
+    }
+}
