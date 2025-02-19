5fbaa7d687721c6ab04e15d60755efb9ccaf04e7
stoftot <gryn@itu.dk>
2025-02-19 22:48:38 +0100

    Made page to create classrooms

---
+++ WebApp-api-sqlserver/Web/Components/Pages/Classrooms/CreateClassroom.razor.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components;
+using Web.Service.DTO_s;
+using Web.Service.Services;
+
+namespace Web.Components.Pages.Classrooms;
+
+public class CreateClassroomBase : ComponentBase
+{
+    [Inject] private NavigationManager Navigation { get; set; } = null!;
+    [Inject] private IClassRoomService ClassRoomService { get; set; } = null!;
+
+    [SupplyParameterFromForm(FormName = "CreateClassroom")]
+    protected CreatFormData FormData { get; set; } = new();
+    
+    protected async Task HandleSubmit()
+    {
+        var newClassRoom = new CreateClassroomDto(FormData.Code, FormData.Capacity);
+        await ClassRoomService.CreateClassroom(newClassRoom);
+        Navigation.NavigateTo($"/classrooms/view");
+    }
+    
+    protected class CreatFormData
+    {
+        [Required(ErrorMessage = "Classroom code is required")]
+        public string Code { get; set; } = string.Empty;
+        
+        [Range(1, 300, ErrorMessage = "Capacity must be between 1 and 300")]
+        public int Capacity { get; set; }
+    }
+}
