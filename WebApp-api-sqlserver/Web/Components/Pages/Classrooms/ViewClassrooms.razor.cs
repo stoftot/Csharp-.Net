993e9d700358a01d720449c21e0bb0f968fc76fe
stoftot <gryn@itu.dk>
2025-02-19 22:48:56 +0100

    Made page to view classrooms

---
+++ WebApp-api-sqlserver/Web/Components/Pages/Classrooms/ViewClassrooms.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Web.Service.DTO_s;
+using Web.Service.Services;
+
+namespace Web.Components.Pages.Classrooms;
+
+public class ViewClassroomsBase : ComponentBase
+{
+    [Inject] private IClassRoomService ClassRoomService { get; init; } = null!;
+
+    protected IEnumerable<ViewClassroomDto>? Classrooms { get; set; } = null;
+
+    protected override async Task OnInitializedAsync()
+    {
+        //await Task.Delay(500);
+        
+        Classrooms = await ClassRoomService.GetAllClassrooms();
+    }
+}
