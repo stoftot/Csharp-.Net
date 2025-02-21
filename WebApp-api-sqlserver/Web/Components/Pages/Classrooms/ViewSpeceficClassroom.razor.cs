1aee2d76315322f832b0525362308619730aedae
stoftot <gryn@itu.dk>
2025-02-21 21:34:13 +0100

    Created page to view, edit and delete a specefic classroom

---
+++ WebApp-api-sqlserver/Web/Components/Pages/Classrooms/ViewSpeceficClassroom.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Web.Service.DTO_s;
+using Web.Service.Services;
+
+namespace Web.Components.Pages.Classrooms;
+
+public class ViewSpeceficClassroomBase : CreateClassroomBase
+{
+    [Parameter] public string ClassroomCode { get; set; }
+
+    [Inject] protected IClassRoomService ClassRoomService { get; set; }
+
+    public ViewClassroomDto? Classroom { get; private set; } = null;
+
+    [SupplyParameterFromForm(FormName = "UpdateClassroom")]
+    protected new CreatFormData UpdateFormData { get; set; } = new();
+
+    protected override async Task OnInitializedAsync()
+    {
+        Classroom = await ClassRoomService.GetClassroom(ClassroomCode);
+        UpdateFormData = new CreatFormData { Code = Classroom.Code, Capacity = Classroom.Capacity };
+    }
+
+    protected async Task UpdateClassRoom()
+    {
+    }
+
+    protected async Task DeleteClassRoom()
+    {
+    }
+}
